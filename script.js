70b11c4025f3883f21fd5437c1386cc478a81e10
Aymen Oumaalla <aymenoml2002@gmail.com>
2025-11-18 12:37:34 +0100

    `Added new room "Spare Room" and corresponding HTML and JavaScript changes`

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 fetch("zones.json")
 .then((res) => res.json())
-.then((roomData) => roomData.forEach(room => {
-        document.querySelector(".row").innerHTML += displayRoom(room)
-    }
-))
+.then((roomData) => {
+    let cardBodies = document.querySelectorAll(".card-body");
+
+    roomData.forEach((room, index) => {
+        if (cardBodies[index]) {
+            cardBodies[index].innerHTML += displayRoom(room);
+        }
+    });
+});
 
 // let zone = document.querySelector(`.room-${room.id}`) 
 // let employeeCount = zone.querySelectorAll(".profile-info").length;
@@ -23,23 +28,19 @@ fetch("employees.json")
             document.querySelector(".room-3").innerHTML += displayReceptionRoomEmployees(employee)
         }else if(employee.location == "Staff Room"){        
             document.querySelector(".room-4").innerHTML += displayStaffRoomEmployees(employee)
+        }else if(employee.location == "Spare Room"){        
+            document.querySelector(".room-5").innerHTML += displayStaffRoomEmployees(employee)
         }
     })
 )
 
 function displayRoom(room) {
         return `
-            <div class="col-12 col-lg-6 col-xl-4">
-                <div class="card bg-card-light shadow-soft h-100 rounded-xl">
-                    <div class="card-body d-flex flex-column">
-                        <div class="d-flex justify-content-between align-items-center mb-2">
-                            <h5 class="card-title mb-0">${room.name}</h5>
-                            <button class="btn btn-link text-dark text-decoration-none p-0"><i class="bi bi-plus-circle"></i>Assign</button>
-                        </div>
-                        <div class="flex-fill room-${room.id}"></div>
-                    </div>
-                </div>
+            <div class="d-flex justify-content-between align-items-center mb-2">
+                <h5 class="card-title mb-0">${room.name}</h5>
+                <button class="btn btn-link text-dark text-decoration-none p-0"><i class="bi bi-plus-circle"></i>Assign</button>
             </div>
+            <div class="flex-fill room-${room.id}"></div>
         `
 }
 
